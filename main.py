6007c91ab8fc53a34cc55c621646eb0143145b9c
Krishtof Korda <krishtofkorda+git@gmail.com>
2017-11-29 21:41:58 -0800

    Change number of epochs to 25

--- main.py
+++ main.py
@@ -275,7 +275,7 @@ def run():
         logits, train_op, cross_entropy_loss = optimize(layer_output, correct_label, learning_rate, num_classes)
 
         # TODO: Train NN using the train_nn function KK-DONE
-        epochs = 20
+        epochs = 25
         batch_size = 4
 
         sess.run(tf.global_variables_initializer())
